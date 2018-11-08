c6d9995f6c5b929523e18f8505972bf66c05b833
jackforez <jackforez@gmail.com>
2018-11-08 21:47:38 +0700

    1

--- resources/views/page/trangchu.blade.php
+++ resources/views/page/trangchu.blade.php
@@ -48,6 +48,86 @@
 </div>
 <div class="row isotope-grid">
 	@foreach($sanpham as $sp)
+	<div class="modal" id="{{$sp->id}}" style="background-color:black;z-index:9999;">
+		<div class="modal-dialog modal-lg w-full">
+			<div class="modal-content">
+				<div class="modal-body">
+					<!-- <div class="row">
+						<div class="col-md-7">
+							<div class="row">
+								<div class="col-md-4">
+									<img src="upload/{{$sp->img}}" alt="hinh 1" width="100%" >	
+								</div>
+								<div class="col-md-4">
+									<img src="upload/{{$sp->img1}}" alt="hinh 2" width="100%" >	
+								</div>
+								<div class="col-md-4">
+									<img src="upload/{{$sp->img2}}" alt="hinh 3" width="100%" >	
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-12">
+									<img src="upload/{{$sp->img}}" alt="hinh 1" width="100%" >	
+								</div>
+								
+							</div>
+						</div>
+						<div class="col-md-5 col-lg-5 p-b-30">
+							<div class="p-r-50 p-t-5 p-lr-0-lg">
+								<h4 class="mtext-105 cl2 js-name-detail p-b-14">
+									{{$sp->tensp}}
+								</h4>
+								<span class="mtext-106 cl2">
+								{{$sp->giasp}}
+								</span>
+								<p class="stext-102 cl3 p-t-23">
+									{{$sp->mota}}
+								</p>
+								<div class="p-t-33">
+									<div class="flex-c-m p-b-10">
+										<div class="size-204 flex-w flex-c-m respon6-next" >
+											<div class="wrap-num-product flex-w m-tb-10" >
+												<div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
+													<i class="fs-16 zmdi zmdi-minus"></i>
+												</div>
+												<input class="mtext-104 cl3 txt-center num-product" type="number" name="num-product" value="1">
+												<div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
+													<i class="fs-16 zmdi zmdi-plus"></i>
+												</div>
+											</div>
+											
+										</div>
+									</div>
+								</div>
+								<div class="flex-w flex-c-m">
+									<button class="btn btn-primary hov-btn1 p-lr-15 trans-04 js-addcart-detail form-control" >
+										Thêm vào giỏ hàng
+									</button>
+								</div>
+							</div>
+						</div>
+					</div> -->
+					<div class="container">
+						<div class="row">
+							<div class="col-2">
+								<ul>
+									<li><img src="upload/{{$sp->img}}" alt="" width="75px"></li>
+									<li><img src="upload/{{$sp->img}}" alt="" width="75px"></li>
+									<li><img src="upload/{{$sp->img}}" alt="" width="75px"></li>
+								</ul>
+							</div>
+							<div class="col-6">
+								<img src="upload/{{$sp->img}}" alt="">	
+							</div>
+							<div class="col-4">
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	<!-- items -->
 	<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item">
 		<!-- Block2 -->
@@ -77,7 +157,7 @@
 		</div>
 	</div>
 	<!-- modal -->
-	<div class="modal" id="{{$sp->id}}" style="background-color:black;z-index:9999;">
+	<!-- <div class="modal" id="{{$sp->id}}" style="background-color:black;z-index:9999;">
 		<div class="modal-dialog modal-lg">
 			<div class="container">
 				<div class="bg0 p-t-60 p-b-30 p-lr-15-lg how-pos3-parent">
@@ -157,8 +237,91 @@
 				</div>
 			</div>
 		</div>
-	</div> 
+	</div>  -->
 	@endforeach 
 </div>
 <div class="row">{{$sanpham->links()}}</div>
+<!-- <div class="modal" id="my-modal" style="background-color:black;z-index:9999;">
+		<div class="modal-dialog modal-lg">
+			<div class="container">
+				<div class="bg0 p-t-60 p-b-30 p-lr-15-lg how-pos3-parent">
+					<button class="how-pos3 hov3 trans-04" data-dismiss="modal">
+					<img src="shop/images/icons/icon-close.png" alt="CLOSE">
+					</button>
+					<div class="row">
+						<div class="col-md-6 col-lg-7 p-b-30">
+							<div class="p-l-25 p-r-30 p-lr-0-lg">
+								<div class="wrap-slick3 flex-sb flex-w">
+									<div class="wrap-slick3-dots"></div>
+									<div class="wrap-slick3-arrows flex-sb-m flex-w"></div>
+									<div class="slick3 gallery-lb" style="background-image: url('upload/{{$sp->img}}');background-repeat: no-repeat;">
+										<div class="item-slick3" data-thumb="">
+											<div class="wrap-pic-w pos-relative" >
+												<img src="" alt="IMG-PRODUCT" id="spimg">
+												<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="upload/{{$sp->img}}">
+												<i class="fa fa-expand"></i>
+												</a>
+											</div>
+										</div>
+										<div class="item-slick3" data-thumb1="">
+											<div class="wrap-pic-w pos-relative">
+												<img src="" id="spimg1">
+												<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="upload/{{$sp->img1}}">
+												<i class="fa fa-expand"></i>
+												</a>
+											</div>
+										</div>
+										<div class="item-slick3" data-thumb2="">
+											<div class="wrap-pic-w pos-relative">
+												<img src="" alt="hình ảnh sản phẩm" id="spimg2">
+												<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="upload/{{$sp->img2}}">
+												<i class="fa fa-expand"></i>
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-lg-5 p-b-30">
+							<div class="p-r-50 p-t-5 p-lr-0-lg">
+								<h4 class="mtext-105 cl2 js-name-detail p-b-14">
+									{{$sp->tensp}}
+								</h4>
+								<span class="mtext-106 cl2">
+								{{$sp->giasp}}
+								</span>
+								<p class="stext-102 cl3 p-t-23">
+									{{$sp->mota}}
+								</p>
+								<div class="p-t-33">
+									<div class="flex-c-m p-b-10">
+										<div class="size-204 flex-w flex-c-m respon6-next" >
+											<div class="wrap-num-product flex-w m-tb-10" >
+												<div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
+													<i class="fs-16 zmdi zmdi-minus"></i>
+												</div>
+												<input class="mtext-104 cl3 txt-center num-product" type="number" name="num-product" value="1">
+												<div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
+													<i class="fs-16 zmdi zmdi-plus"></i>
+												</div>
+											</div>
+											
+										</div>
+									</div>
+								</div>
+								<div class="flex-w flex-c-m">
+									<button class="btn btn-primary hov-btn1 p-lr-15 trans-04 js-addcart-detail form-control" >
+										Thêm vào giỏ hàng
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>  -->
+@endsection
+@section('my-script')
 @endsection
